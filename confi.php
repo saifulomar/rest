97cf3f83ade4f9fa1409f9c5df790217cab612e3
Saiful Omar <omar.saiful@gmail.com>
2015-08-19 23:33:15 +0800

    third commit

--- confi.php
+++ confi.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysql_connect("127.5.123.130:3306", "admin1qC8LT2", "fkV2843jer3d");
+$conn = mysql_connect("127.5.123.130", "admin1qC8LT2", "fkV2843jer3d");
 mysql_select_db('tuts_rest', $conn);
